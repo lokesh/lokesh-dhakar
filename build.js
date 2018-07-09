1afc698a20e0aee336811aad5fb81c672abbaacc
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2018-07-08 19:57:48 -0700

    remove autoprefixer and sass compilation

--- build.js
+++ build.js
@@ -1,10 +1,9 @@
-require('harmonize')();
+// require('harmonize')();
 
 // Metalsmith
 var metalsmith = require('metalsmith');
 
 // Metalsmith Plugins
-var autoprefixer = require('metalsmith-autoprefixer');
 var layouts = require('metalsmith-layouts');
 var markdown = require('metalsmith-markdown');
 var permalinks = require('metalsmith-permalinks');
@@ -16,20 +15,24 @@ var siteBuild = metalsmith(__dirname)
   .metadata({
     site: {
       title: 'Lokesh Dhakar',
-      url: 'http://lokeshdhakar.com'
+      url: 'https://lokeshdhakar.com'
     }
   })
+
   .source('./src')
   .destination('./dist')
+
+  .clean(true) // Clean destination dir on build
   .use(sass({
     outputDir: 'css/',
     outputStyle: 'expanded'
   }))
-  .use(autoprefixer())
+
+  // .use(drafts()) // Only files that are NOT drafts.
+
   .use(markdown())
   .use(permalinks(':title'))
   .use(layouts({
-    engine: 'jade',
     directory: 'src/layouts'
   }))
   .use(serve())
@@ -39,7 +42,7 @@ var siteBuild = metalsmith(__dirname)
         '${source}/**/*': true,
         // When a SASS or JS file is edited, force a complete rebuild. I was not able to get
         // metalsmith-watch to rebuild just the CSS on a SASS edit or just the JS.
-        '${source}/sass/**/*': '**/*',
+        '${source}/css/**/*': '**/*',
         '${source}/js/**/*': '**/*',
         '${source}/layouts/**/*': '**/*'
       },
